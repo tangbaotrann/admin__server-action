c710c89d0a9eb3b624cdb7a8ec5033ef0fd881ca
tangbaotrann <tangbaotrann@gmail.com>
2024-02-08 01:36:18 +0700

    updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
