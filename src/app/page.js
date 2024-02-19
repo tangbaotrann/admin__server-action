f432bd8331df7cebbf44368c460704b5ef599bf0
tangbaotrann <tangbaotrann@gmail.com>
2024-02-20 00:14:09 +0700

    updated: deleted cicd

--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,6 @@ export default function Home() {
       <p>Home hi! nextjs@14 cicd</p>
       <p>Home hi! nextjs@14 cicd</p>
       <p>Home hi! nextjs@14 cicd</p>
-      <p>Home hi! nextjs@14 cicd</p>
-      <p>Home hi! nextjs@14 cicd</p>
-      <p>Home hi! nextjs@14 cicd</p>
-      <p>Home hi! nextjs@14 cicd</p>
-      <p>Home hi! nextjs@14 cicd</p>
 
       <Link href="/dashboard">Go to Dashboard now.</Link>
     </div>
